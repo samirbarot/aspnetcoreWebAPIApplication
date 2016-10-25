bd522880ee52faaa5dad1aa8d4c53430604c5020
Barot <sb35650@nsroot.net>
2016-10-25 17:36:10 -0400

    first commit

---
+++ src/ASPNETCoreWebAPI/Controllers/DataController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class DataController : Controller
+    {
+
+        [Route("/api/hello/{name}")]
+        [HttpGet]
+        public string Hello(string name) => $"Returning response from CORS Web API v4 - { name }";
+
+        // GET api/data
+        [HttpGet]
+        public IEnumerable<string> Get() => new string[] { "value1", "value2" };
+
+        // GET api/data/5
+        [HttpGet("{id}")]
+        public string Get(int id) => "value";
+
+        // POST api/data
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/data/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/data/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/ASPNETCoreWebAPI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace ASPNETCoreWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                    .AddCommandLine(args)
+                    .Build();
+
+            var hostUrl = configuration["hosturl"];
+            if (string.IsNullOrEmpty(hostUrl))
+                hostUrl = "http://0.0.0.0:6000";
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls(hostUrl)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
